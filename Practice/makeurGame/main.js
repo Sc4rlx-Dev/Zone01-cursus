efffa5d0ceaa3664fc0ac55a99cba0fd7092d93b
oerraoui <itsscarl2022@gmail.com>
2025-07-16 18:25:57 +0100

    add window

--- Practice/makeurGame/main.js
+++ Practice/makeurGame/main.js
@@ -1,3 +1,4 @@
+
 const bt1 = document.getElementById("start")
 const bt2 = document.getElementById("end")
 const imag = document.getElementById("image")
@@ -9,24 +10,34 @@ const src = document.getElementById('header')
 
 bt1.addEventListener('click', () => {
     if (!document.querySelector('#header img')) {
+        alert('Game Started')
         const img = document.createElement('img')
         img.src = "./img.png"
         img.style.width = '350px'
         src.innerHTML = ''
         src.appendChild(img)
-        flag = true
+        Game_started = true
     }
 })
 
-bt2.addEventListener('click', () => {
+function close() {
+    bt2.addEventListener('click', () => {
     src.innerHTML = ''
 })
+}
 
 if(Game_started == true){
     const newGame = document.getElementById("new")
     const startgame = document.getElementById("display")
 
-    newGame.addEventListener('click' , )
+
+
+    newGame.addEventListener('click' , ()=> {
+        const window = document.createElement('img2')
+        img2.src = "./phone.jpg"
+        const phone = getElementById('header')
+        phone.removeChild(src)
+    })
 }
 
 
